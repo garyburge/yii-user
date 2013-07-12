d6b1346bf97341e5750c0e97a4e8ff69bb09a393
Gary Burge <garyburge@garyburge.com>
2013-07-12 05:20:53 -0400

    Comment out AdminController::actionDelete explicit profile->delete() to test foreign key on_delete constraint

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 
-  class AdminController extends Controller {
+class AdminController extends Controller
+{
 
     public $defaultAction = 'admin';
     private $_model;
@@ -8,10 +9,11 @@
     /**
      * @return array action filters
      */
-    public function filters() {
-      return CMap::mergeArray(parent::filters(), array(
-          'accessControl', // perform access control for CRUD operations
-      ));
+    public function filters()
+    {
+        return CMap::mergeArray(parent::filters(), array(
+            'accessControl', // perform access control for CRUD operations
+        ));
     }
 
     /**
@@ -19,154 +21,163 @@
      * This method is used by the 'accessControl' filter.
      * @return array access control rules
      */
-    public function accessRules() {
-      return array(
-        array('allow', // allow admin user to perform 'admin' and 'delete' actions
-          'actions' => array('admin', 'delete', 'create', 'update', 'view'),
-          'users' => UserModule::getAdmins(),
-        ),
-        array('deny', // deny all users
-          'users' => array('*'),
-        ),
-      );
+    public function accessRules()
+    {
+        return array(
+            array('allow', // allow admin user to perform 'admin' and 'delete' actions
+                'actions'=>array('admin', 'delete', 'create', 'update', 'view'),
+                'users'=>UserModule::getAdmins(),
+            ),
+            array('deny', // deny all users
+                'users'=>array('*'),
+            ),
+        );
     }
 
     /**
      * Manages all models.
      */
-    public function actionAdmin() {
-      $model = new User('search');
-      $model->unsetAttributes();  // clear any default values
-      if (isset($_GET['User']))
-        $model->attributes = $_GET['User'];
+    public function actionAdmin()
+    {
+        $model = new User('search');
+        $model->unsetAttributes();  // clear any default values
+        if (isset($_GET['User']))
+            $model->attributes = $_GET['User'];
 
-      $this->render('index', array(
-        'model' => $model,
-      ));
-      /* $dataProvider=new CActiveDataProvider('User', array(
-        'pagination'=>array(
-        'pageSize'=>Yii::app()->controller->module->user_page_size,
-        ),
+        $this->render('index', array(
+            'model'=>$model,
         ));
+        /* $dataProvider=new CActiveDataProvider('User', array(
+          'pagination'=>array(
+          'pageSize'=>Yii::app()->controller->module->user_page_size,
+          ),
+          ));
 
-        $this->render('index',array(
-        'dataProvider'=>$dataProvider,
-        ));// */
+          $this->render('index',array(
+          'dataProvider'=>$dataProvider,
+          ));// */
     }
 
     /**
      * Displays a particular model.
      */
-    public function actionView() {
-      $model = $this->loadModel();
-      $this->render('view', array(
-        'model' => $model,
-      ));
+    public function actionView()
+    {
+        $model = $this->loadModel();
+        $this->render('view', array(
+            'model'=>$model,
+        ));
     }
 
     /**
      * Creates a new model.
      */
-    public function actionCreate() {
-      $model = new User;
-      $profile = new Profile;
-      $this->performAjaxValidation(array($model, $profile));
-      if (isset($_POST['User'])) {
-        $model->attributes = $_POST['User'];
-        $model->activeKey = Yii::app()->controller->module->encrypting(microtime() . $model->password);
-        $profile->attributes = $_POST['Profile'];
-        $profile->user_id = 0;
-        if ($model->validate() && $profile->validate()) {
-          $model->password = Yii::app()->controller->module->encrypting($model->password);
-          if ($model->save()) {
-            $profile->user_id = $model->id;
-            $profile->save();
-          }
-          $this->redirect(array('admin'));
+    public function actionCreate()
+    {
+        $model = new User;
+        $profile = new Profile;
+        $this->performAjaxValidation(array($model, $profile));
+        if (isset($_POST['User'])) {
+            $model->attributes = $_POST['User'];
+            $model->activeKey = Yii::app()->controller->module->encrypting(microtime() . $model->password);
+            $profile->attributes = $_POST['Profile'];
+            $profile->user_id = 0;
+            if ($model->validate() && $profile->validate()) {
+                $model->password = Yii::app()->controller->module->encrypting($model->password);
+                if ($model->save()) {
+                    $profile->user_id = $model->id;
+                    $profile->save();
+                }
+                $this->redirect(array('admin'));
+            }
+            else
+                $profile->validate();
         }
-        else
-          $profile->validate();
-      }
 
-      $this->render('create', array(
-        'model' => $model,
-        'profile' => $profile,
-      ));
+        $this->render('create', array(
+            'model'=>$model,
+            'profile'=>$profile,
+        ));
     }
 
     /**
      * Updates a particular model.
      */
-    public function actionUpdate() {
-      $model = $this->loadModel();
-      $profile = $model->profile;
-      $this->performAjaxValidation(array($model, $profile));
-      if (isset($_POST['User'])) {
-        $model->attributes = $_POST['User'];
-        $profile->attributes = $_POST['Profile'];
+    public function actionUpdate()
+    {
+        $model = $this->loadModel();
+        $profile = $model->profile;
+        $this->performAjaxValidation(array($model, $profile));
+        if (isset($_POST['User'])) {
+            $model->attributes = $_POST['User'];
+            $profile->attributes = $_POST['Profile'];
 
-        if ($model->validate() && $profile->validate()) {
-          $old_password = User::model()->notsafe()->findByPk($model->id);
-          if ($old_password->password != $model->password) {
-            $model->password = Yii::app()->controller->module->encrypting($model->password);
-            $model->activeKey = Yii::app()->controller->module->encrypting(microtime() . $model->password);
-          }
-          $model->save();
-          $profile->save();
-          $this->redirect(array('admin'));
+            if ($model->validate() && $profile->validate()) {
+                $old_password = User::model()->notsafe()->findByPk($model->id);
+                if ($old_password->password != $model->password) {
+                    $model->password = Yii::app()->controller->module->encrypting($model->password);
+                    $model->activeKey = Yii::app()->controller->module->encrypting(microtime() . $model->password);
+                }
+                $model->save();
+                $profile->save();
+                $this->redirect(array('admin'));
+            }
+            else
+                $profile->validate();
         }
-        else
-          $profile->validate();
-      }
 
-      $this->render('update', array(
-        'model' => $model,
-        'profile' => $profile,
-      ));
+        $this->render('update', array(
+            'model'=>$model,
+            'profile'=>$profile,
+        ));
     }
 
     /**
      * Deletes a particular model.
      * If deletion is successful, the browser will be redirected to the 'index' page.
      */
-    public function actionDelete() {
-      if (Yii::app()->request->isPostRequest) {
-        // we only allow deletion via POST request
-        $model = $this->loadModel();
-        $profile = Profile::model()->findByPk($model->id);
-        $profile->delete();
-        $model->delete();
-        // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
-        if (!isset($_POST['ajax']))
-          $this->redirect(array('/user/admin'));
-      } else {
-        throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
-      }
+    public function actionDelete()
+    {
+        if (Yii::app()->request->isPostRequest) {
+            // we only allow deletion via POST request
+            $model = $this->loadModel();
+//            $profile = Profile::model()->findByPk($model->id);
+//            $profile->delete();
+            $model->delete();
+            // if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
+            if (!isset($_POST['ajax'])) {
+                $this->redirect(array('/user/admin'));
+            }
+        } else {
+            throw new CHttpException(400, 'Invalid request. Please do not repeat this request again.');
+        }
     }
 
     /**
      * Performs the AJAX validation.
      * @param CModel the model to be validated
      */
-    protected function performAjaxValidation($validate) {
-      if (isset($_POST['ajax']) && $_POST['ajax'] === 'user-form') {
-        echo CActiveForm::validate($validate);
-        Yii::app()->end();
-      }
+    protected function performAjaxValidation($validate)
+    {
+        if (isset($_POST['ajax']) && $_POST['ajax'] === 'user-form') {
+            echo CActiveForm::validate($validate);
+            Yii::app()->end();
+        }
     }
 
     /**
      * Returns the data model based on the primary key given in the GET variable.
      * If the data model is not found, an HTTP exception will be raised.
      */
-    public function loadModel() {
-      if ($this->_model === null) {
-        if (isset($_GET['id']))
-          $this->_model = User::model()->notsafe()->findbyPk($_GET['id']);
-        if ($this->_model === null)
-          throw new CHttpException(404, 'The requested page does not exist.');
-      }
-      return $this->_model;
+    public function loadModel()
+    {
+        if ($this->_model === null) {
+            if (isset($_GET['id']))
+                $this->_model = User::model()->notsafe()->findbyPk($_GET['id']);
+            if ($this->_model === null)
+                throw new CHttpException(404, 'The requested page does not exist.');
+        }
+        return $this->_model;
     }
 
-  }
+}
