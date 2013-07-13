0d14c0964dcd80ab1dde4fb0d736677dbd471d7a
Gary Burge <garyburge@garyburge.com>
2013-07-13 11:38:59 -0400

    Returned yii::trace calls for debugging

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -27,6 +27,7 @@ class LoginController extends Controller
 
             // display the login form
             $this->render('/user/login', array('model'=>$model));
+
         } else {
             // user already logged in; goto normal return page
             $this->redirectToReturnUrl();
@@ -48,6 +49,8 @@ class LoginController extends Controller
      */
     private function redirectToReturnUrl()
     {
+        Yii::trace(__METHOD__ . " (" . __LINE__ . "): Yii::app()->controller->module->returnUrl=".print_r(Yii::app()->controller->module->returnUrl, true), 'user');
+        Yii::trace(__METHOD__ . " (" . __LINE__ . "): Yii::app()->user->returnUrl=".print(Yii::app()->user->returnUrl, true), 'user');
         if (isset(Yii::app()->controller->module->returnUrl) && !empty(Yii::app()->controller->module->returnUrl)) {
             $this->redirect(Yii::app()->controller->module->returnUrl);
         } else if (isset(Yii::app()->user->returnUrl) && !empty(Yii::app()->user->returnUrl)) {
