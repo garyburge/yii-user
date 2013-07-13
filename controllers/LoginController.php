f5279089a9e46d0d54538b1b12b5f5312168ba15
Gary Burge <garyburge@garyburge.com>
2013-07-13 11:32:21 -0400

    Separated redirect to return url to separate, private function

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,41 +1,63 @@
 <?php
 
-  class LoginController extends Controller {
+class LoginController extends Controller
+{
 
     public $defaultAction = 'login';
 
     /**
      * Displays the login page
      */
-    public function actionLogin() {
-      if (Yii::app()->user->isGuest) {
-        $model = new UserLogin;
-        // collect user input data
-        if (isset($_POST['UserLogin'])) {
-          $model->attributes = $_POST['UserLogin'];
-          // validate user input and redirect to previous page if valid
-          if ($model->validate()) {
-            $this->lastVisit();
-            Yii::trace(__METHOD__ . " (" . __LINE__ . "): Yii::app()->controller->module->returnUrl='".print_r(Yii::app()->controller->module->returnUrl, true)."'", 'user');
-            Yii::trace(__METHOD__ . " (" . __LINE__ . "): Yii::app()->user->returnUrl='".print_r(Yii::app()->user->returnUrl, true)."'", 'user');
-            $this->redirect(Yii::app()->controller->module->returnUrl);
-//            if (Yii::app()->user->returnUrl == '/index.php')
-//              $this->redirect(Yii::app()->controller->module->returnUrl);
-//            else
-//              $this->redirect(Yii::app()->user->returnUrl);
-          }
+    public function actionLogin()
+    {
+        if (Yii::app()->user->isGuest) {
+            // create login form model
+            $model = new UserLogin;
+
+            // if form data available
+            if (isset($_POST['UserLogin'])) {
+                // copy form data to model
+                $model->attributes = $_POST['UserLogin'];
+                // validate user input and redirect to previous page if valid
+                if ($model->validate()) {
+                    $this->lastVisit();
+                    $this->redirectToReturnUrl();
+                }
+            }
+
+            // display the login form
+            $this->render('/user/login', array('model'=>$model));
+        } else {
+            // user already logged in; goto normal return page
+            $this->redirectToReturnUrl();
         }
-        // display the login form
-        $this->render('/user/login', array('model' => $model));
-      }
-      else
-        $this->redirect(Yii::app()->controller->module->returnUrl);
     }
 
-    private function lastVisit() {
-      $lastVisit = User::model()->notsafe()->findByPk(Yii::app()->user->id);
-      $lastVisit->lastvisit = time();
-      $lastVisit->save();
+    /**
+     * update last visit colum with current date and time
+     */
+    private function lastVisit()
+    {
+        $lastVisit = User::model()->notsafe()->findByPk(Yii::app()->user->id);
+        $lastVisit->lastvisit_at = time();
+        $lastVisit->save();
     }
 
-  }
+    /**
+     * redirect to defined return url
+     */
+    private function redirectToReturnUrl()
+    {
+        Yii::trace(__METHOD__ . " (" . __LINE__ . "): Yii::app()->controller->module->returnUrl='" . print_r(Yii::app()->controller->module->returnUrl, true) . "'", 'user');
+        Yii::trace(__METHOD__ . " (" . __LINE__ . "): Yii::app()->user->returnUrl='" . print_r(Yii::app()->user->returnUrl, true) . "'", 'user');
+
+        if (isset(Yii::app()->controller->module->returnUrl) && !empty(Yii::app()->controller->module->returnUrl)) {
+            $this->redirect(Yii::app()->controller->module->returnUrl);
+        } else if (isset(Yii::app()->user->returnUrl) && !empty(Yii::app()->user->returnUrl)) {
+            $this->redirect(Yii::app()->user->returnUrl);
+        } else {
+            $this->redirect('/');
+        }
+    }
+
+}
