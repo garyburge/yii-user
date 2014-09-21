98de848a43bd59731fbdf5618c01251d84e9eed5
Gary Burge <garyburge@garyburge.com>
2014-09-21 07:15:21 -0400

    Update registration.php
    Convert to Yii-Booster and bootstrap 3

--- views/user/registration.php
+++ views/user/registration.php
@@ -25,30 +25,32 @@ $this->breadcrumbs=array(
     <p class="muted"><small><?php echo UserModule::t('Fields with <span class="required">*</span> are required.'); ?></small></p>
     <?php echo $form->errorSummary(array($model,$profile)); ?>
 
-    <?php echo $form->textFieldRow($model, 'username'); ?>
-    <?php echo $form->passwordFieldRow($model, 'password'); ?>
+    <?php echo $form->textFieldGroup($model, 'username'); ?>
+    <?php echo $form->passwordGroup($model, 'password'); ?>
     <p class="help-block"><?php echo UserModule::t("Minimal password length 4 symbols."); ?></p>
-    <?php echo $form->passwordFieldRow($model, 'verifyPassword'); ?>
-    <?php echo $form->textFieldRow($model, 'email', array('class'=>'span4')); ?>
+    <?php echo $form->passwordFieldGroup($model, 'verifyPassword'); ?>
+    <?php echo $form->textFieldGroup($model, 'email'); ?>
 
     <?php
       $profileFields = $profile->getFields();
       if ($profileFields) {
         foreach($profileFields as $field) {
     ?>
-      <?php echo $form->labelEx($profile,$field->varname); ?>
+      <?php //echo $form->labelEx($profile,$field->varname); ?>
       <?php
         if ($widgetEdit = $field->widgetEdit($profile)) {
-          echo $widgetEdit;
+        	echo $form->labelEx($profile,$field->varname)."<br>";
+          echo $widgetEdit."<br>";
+          echo $form->error($profile,$field->varname)."<br>";
         } elseif ($field->range) {
-          echo $form->dropDownList($profile, $field->varname, Profile::range($field->range));
+          echo $form->dropDownListGroup($profile, $field->varname, Profile::range($field->range));
         } elseif ($field->field_type == "TEXT") {
-          echo$form->textArea($profile, $field->varname, array('rows'=>6, 'cols'=>50));
+          echo$form->textAreaGroup($profile, $field->varname, array('rows'=>6));
         } else {
-          echo $form->textField($profile, $field->varname, array('size'=>60, 'maxlength'=>(($field->field_size) ? $field->field_size : 255)));
+          echo $form->textFieldGroup($profile, $field->varname, array('size'=>60, 'maxlength'=>(($field->field_size) ? $field->field_size : 255)));
         }
       ?>
-      <?php echo $form->error($profile,$field->varname); ?>
+      <?php //echo $form->error($profile,$field->varname); ?>
     <?php
         }
       }
