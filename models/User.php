66a9df008ae3f5400af57d7f560c5291f99dbb79
Gary Burge <garyburge@garyburge.com>
2014-09-21 07:54:40 -0400

    Update User.php
    Again, fixed username, password minimum, maximum lengths and messages

--- models/User.php
+++ models/User.php
@@ -49,7 +49,7 @@ class User extends CActiveRecord
       // will receive user inputs.CConsoleApplication
       return ((get_class(Yii::app()) == 'CConsoleApplication' || (get_class(Yii::app()) != 'CConsoleApplication' && Yii::app()->getModule('user')->isAdmin())) ? array(
         array('username', 'length', 'max'=>20, 'min'=>8, 'message'=>UserModule::t("Incorrect username (length between 8 and 20 characters).")),
-        array('password', 'length', 'max'=>128, 'min'=>8, 'message'=>UserModule::t("Incorrect password (minimal length 8 symbols).")),
+        array('password', 'length', 'max'=>128, 'min'=>8, 'message'=>UserModule::t("Incorrect password (minimum length is 8 character).")),
         array('email', 'email'),
         array('username', 'unique', 'message'=>UserModule::t("This user's name already exists.")),
         array('email', 'unique', 'message'=>UserModule::t("This user's email address already exists.")),
@@ -63,7 +63,7 @@ class User extends CActiveRecord
         array('id, username, password, email, activeKey, create_at, lastvisit_at, superuser, status', 'safe', 'on'=>'search'),
       ) : ((Yii::app()->user->id == $this->id) ? array(
         array('username, email', 'required', 'except'=>'search'),
-        array('username', 'length', 'max'=>20, 'min'=>3, 'message'=>UserModule::t("Incorrect username (length between 3 and 20 characters).")),
+        array('username', 'length', 'max'=>20, 'min'=>8, 'message'=>UserModule::t("Incorrect username (length between 8 and 20 characters).")),
         array('email', 'email'),
         array('username', 'unique', 'message'=>UserModule::t("This user's name already exists.")),
         array('username', 'match', 'pattern'=>'/^[A-Za-z0-9_]+$/u', 'message'=>UserModule::t("Incorrect symbols (A-z0-9).")),
